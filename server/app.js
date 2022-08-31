688763fb1d6fa66c7a55576929b642feb0135e7a
LevKoganov <levk@playsmart.co.il>
2022-08-31 13:51:40 +0300

    test

--- server/app.js
+++ server/app.js
@@ -29,12 +29,12 @@ app.use('/login', loginRouter);
 app.use('/posts', postRouter);
 app.use('/users', usersRouter);
 
+
 // Production env
-if(process.env.NODE_ENV === "production") {
-  app.use(express.static("/client/build"));
-  app.get("*", (req,res) => {
-    res.sendFile(path.resolve(__dirname, '/client', "build", "index.html"))
-  })
-}
+app.use(express.static("../client/build"));
+app.get("*", (req,res) => {
+  res.sendFile(path.resolve(__dirname, '../client', "build", "index.html"))
+})
+
 
 module.exports = app;
